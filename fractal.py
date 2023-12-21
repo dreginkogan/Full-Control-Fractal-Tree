c5f3659e81d0c6a6771fd0964ac4ba067d25b58d
Danya Kogan <75654428+dreginkogan@users.noreply.github.com>
2023-12-22 00:09:00 +0100

    3D tree generates, but there are still some trigonometry errors

--- fractal.py
+++ fractal.py
@@ -2,7 +2,7 @@ import fullcontrol as fc
 from math import tau, sin, cos, tan, radians, pi
 #import numpy as np
 
-def grow2(currPt, minLength=2, currAng = 90, length=20, splitAngle = 30):
+def grow2(currPt, minLength=2, currAng = 90, length=20, splitAngle = 45):
     steps = [fc.Extruder(on=False),currPt]
 
     if length>minLength: # terminates at this angle
@@ -25,29 +25,32 @@ def grow2(currPt, minLength=2, currAng = 90, length=20, splitAngle = 30):
 
     return steps # steps
 
-def grow3(currPt, minLength=2, currTheta = 0, currPhi = 0, length=20, splitTheta = 15):
+def grow3(currPt, minLength=2, currTheta = 0, currPhi = 0, length=128, splitTheta = 70): # gonna hard code the 3 branches, will make it expandable later
     steps = [fc.Extruder(on=False),currPt]
 
     if length>minLength: # terminates at this angle
         steps.append(fc.Extruder(on=True))
 
-        currHStep = sin(radians(currAng))*length 
-        currZStep = cos(radians(currAng))*length
+        currHStep = sin(radians(currTheta))*length 
+        currZStep = cos(radians(currTheta))*length
 
-        steps.append(fc.Point(x=currPt.x + cos(radians(currPhi))*currHStep,y=currPt.y + cos(radians(currPhi))*currHStep,z=currPt.z + currZStep))
+        steps.append(fc.Point(x=currPt.x + sin(radians(currPhi))*currHStep,y=currPt.y + cos(radians(currPhi))*currHStep,z=currPt.z + currZStep))
 
-        newLength = length*0.75
-        leftAngle = currAng + splitAngle/2
-        rightAngle = currAng - splitAngle/2
+        newLength = length*0.5 # how much length shrinks by
+        
 
         # left
-        steps.extend(grow2(fc.Point(x=currPt.x + currXStep,y=0,z=currPt.z + currZStep), currAng = leftAngle, length=newLength))
-        
+        steps.extend(grow3(fc.Point(x=currPt.x + sin(radians(currPhi))*currHStep,y=currPt.y + cos(radians(currPhi))*currHStep,z=currPt.z + currZStep), currTheta = currTheta + splitTheta, currPhi = currPhi+0, length=newLength))
+    
         # right top
-        steps.extend(grow2(fc.Point(=currPt.x + cos(radians(currPhi))*currHStep,y=currPt.y + cos(radians(currPhi))*currHStep,z=currPt.z + currZStep), currAng = rightAngle, length=newLength))
+        steps.extend(grow3(fc.Point(x=currPt.x + sin(radians(currPhi))*currHStep,y=currPt.y + cos(radians(currPhi))*currHStep,z=currPt.z + currZStep), currTheta = currTheta + splitTheta, currPhi = currPhi+120, length=newLength))
+
+        # right bottom
+        steps.extend(grow3(fc.Point(x=currPt.x + sin(radians(currPhi))*currHStep,y=currPt.y + cos(radians(currPhi))*currHStep,z=currPt.z + currZStep), currTheta = currTheta + splitTheta, currPhi = currPhi-120, length=newLength))
 
     return steps # steps
 
-steps = grow2(fc.Point(x=0,y=0,z=0))
+steps = grow3(fc.Point(x=0,y=0,z=0))
+# steps = grow3(fc.Point(x=0,y=0,z=0))
 # steps = np.append(steps, fc.PlotAnnotation(steps[0], label="point 1"))
 fc.transform(steps, 'plot', fc.PlotControls(color_type='print_sequence', style='line'))
