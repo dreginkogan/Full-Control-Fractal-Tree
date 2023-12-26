d36a15b32f6fe707730db224a393bc42c6e5fdc2
Danya Kogan <75654428+dreginkogan@users.noreply.github.com>
2023-12-26 19:24:07 +0100

    im dying from this. i need to figure out the trig/linalg, this is all too tedious.

--- fractal.py
+++ fractal.py
@@ -11,7 +11,7 @@ def grow2(currPt, minLength=2, currAng = 90, length=20, splitAngle = 45):
         currXStep = cos(radians(currAng))*length
         currZStep = sin(radians(currAng))*length
 
-        steps.append(fc.Point(x=currPt.x + currXStep,y=0,z=currPt.z + currZStep))
+        steps.append(fc.Point(x=currPt.x + currXStep,y=0,z=currPt.z + currZStep)) # shouldnt have to add offsets to this
 
         newLength = length*0.75
         leftAngle = currAng + splitAngle/2
@@ -25,72 +25,42 @@ def grow2(currPt, minLength=2, currAng = 90, length=20, splitAngle = 45):
 
     return steps # steps
 
-# def grow3(currPt, minLength=2, currTheta = 0, currPhi = 0, length=128, splitTheta = 30): # gonna hard code the 3 branches, will make it expandable later
-#     steps = [fc.Extruder(on=False),currPt]
 
-#     if length>minLength: # terminates at this angle
-#         steps.append(fc.Extruder(on=True))
+## needs to be written with relative spherical coordinates
+def grow4(currPt, minLength=2, theta = 0, phi = 0, length=128, splitAngle = 60):# each axis alone works, but together it fucks up
+    # issue is probably with going to the proper base location
 
-#         currHStep = sin(radians(currTheta))*length # component of length on xy plane
+    steps = [currPt, fc.Extruder(on=False)]
 
-#         currZStep = cos(radians(currTheta))*length # to determine seed position
+    if length>minLength: # terminates at this angle 
+        steps.append(fc.Extruder(on=True))
 
-#         # rTZ = 
-#         # rBZ =  
+        currXStep = sin(radians(theta))*cos(radians(phi))*length
+        currYStep = sin(radians(theta))*sin(radians(phi))*length
+        currZStep = cos(radians(theta))*length
 
-#         # rTX =
-#         # rBX =  
+        newLength = length*0.5
 
-#         # rTY = 
-#         # rBY = 
+        # leftPitch = currPitch - splitAngle/2
+        # rightPitch = currPitch + splitAngle/2
 
-#         # to draw singular line to here, the point where the other branches will seed from
-#         steps.append(fc.Point(x=currPt.x + cos(radians(currPhi))*currHStep ,y=currPt.y + sin(radians(currPhi))*currHStep,z=currPt.z + currZStep))
+        # botRoll = currRoll - splitAngle/2
+        # topRoll = currRoll + splitAngle/2
 
-#         newLength = length*0.5 # how much length shrinks by
-        
+        # left
+        steps.extend(grow4(fc.Point(x=currPt.x + currXStep,y=currPt.y + currYStep,z=currPt.z + currZStep), theta = theta-splitAngle/2*cos(radians(theta)), phi = 0, length=newLength))
+        # right
+        steps.extend(grow4(fc.Point(x=currPt.x + currXStep,y=currPt.y + currYStep,z=currPt.z + currZStep), theta = theta+splitAngle/2*cos(radians(theta)), phi = 0, length=newLength))
+        # top
+        # steps.extend(grow4(fc.Point(x=currPt.x,y=currPt.y + currYStep,z=currPt.z + currZStep), currRoll = topRoll, length=newLength))
+        # bottom
+        # steps.extend(grow4(fc.Point(x=currPt.x,y=currPt.y + currYStep,z=currPt.z + currZStep), currRoll = botRoll, length=newLength))
 
-#         # right # does not need phi
-#         steps.extend(grow3(fc.Point(x=currPt.x + currHStep * cos(0),y=currPt.y + currHStep * sin(0), z=currPt.z + currZStep), currTheta = currTheta + splitTheta, currPhi = currPhi+0, length=newLength))
-    
-#         # left top
-#         # steps.extend(grow3(fc.Point(x=currPt.x + cos(radians(currPhi-120))*currHStep,y=currPt.y + sin(radians(currPhi-120))*currHStep,z=currPt.z + currZStep), currTheta = currTheta + splitTheta, currPhi = currPhi+120, length=newLength))
+        steps.append(fc.Point(x=currPt.x,y=currPt.y,z=currPt.z)) # go back to origin
 
-#         # left bottom
-#         # steps.extend(grow3(fc.Point(x=currPt.x + cos(radians(currPhi+120))*currHStep,y=currPt.y + sin(radians(currPhi+120))*currHStep,z=currPt.z + currZStep), currTheta = currTheta + splitTheta, currPhi = currPhi-120, length=newLength))
+    return steps # steps
 
-#     return steps # steps
-
-# def grow4(currPt, minLength=2, currAng = 90, length=128, splitAngle = 45):
-#     steps = [fc.Extruder(on=False),currPt]
-
-#     if length>minLength: # terminates at this angle
-#         steps.append(fc.Extruder(on=True))
-
-#         currXStep = cos(radians(currAng))*length
-#         currZStep = sin(radians(currAng))*length
-
-#         steps.append(fc.Point(x=currPt.x ,y=currPt.y,z=currPt.z)) # the next seed location
-
-#         newLength = length*0.5
-#         leftAngle = currAng + splitAngle/2
-#         rightAngle = currAng - splitAngle/2
-
-#         # left
-#         steps.extend(grow4(fc.Point(x=currPt.x + currXStep,y=currPt.y,z=currPt.z + currZStep), currAng = leftAngle, length=newLength))
-        
-#         # right
-#         steps.extend(grow4(fc.Point(x=currPt.x + currXStep,y=currPt.y,z=currPt.z + currZStep), currAng = rightAngle, length=newLength))
-
-#         # top
-#         steps.extend(grow4(fc.Point(x=currPt.x,y=currPt.y + currXStep,z=currPt.z + currZStep), currAng = rightAngle, length=newLength))
-
-#         # bottom
-#         steps.extend(grow4(fc.Point(x=currPt.x,y=currPt.y + currXStep,z=currPt.z + currZStep), currAng = leftAngle, length=newLength))
-
-#     return steps # steps
-
-steps = grow2(fc.Point(x=0,y=0,z=0))
+steps = grow4(fc.Point(x=0,y=0,z=0), theta = 0, phi = 0)
 # steps = grow2(fc.Point(x=0,y=0,z=0))
 # steps = np.append(steps, fc.PlotAnnotation(steps[0], label="point 1"))
 fc.transform(steps, 'plot', fc.PlotControls(color_type='print_sequence', style='line'))
